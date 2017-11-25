373c812527bb00f9032c4a7781f6a316d0cc0f09
Blax_ <adlani.mou3taz@gmail.com>
2017-11-25 23:28:19 +0100

    Landing page + page products

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -9,11 +9,20 @@ use Symfony\Component\HttpFoundation\Request;
 class BlogController extends Controller
 {
     /**
-     * @Route("/article/", name="Article")
+     * @Route("/article/{id}", name="Article" , requirements={"id": "\d+"})
      */
-    public function productAction()
+    public function productAction($id)
     {
         // replace this example code with whatever you need
-        return $this->render('blog/product.html.twig', []);
+        return $this->render('blog/product.html.twig', ['id_product' => $id]);
+    }
+
+    /**
+     * @Route("/articles/", name="Articles" )
+     */
+    public function productsAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('blog/products.html.twig', []);
     }
 }
